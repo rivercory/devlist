59450be17897d6f1edcf2be8993070b4810b9741
hyeonho64 <hyeonhokang10@kakao.com>
2023-12-03 13:43:13 +0900

    Version 1.1.5

--- src/App.js
+++ src/App.js
@@ -1,42 +1,50 @@
 import './App.css';
-import { Link, Route, Routes } from 'react-router-dom';
-import { HiBars3 } from "react-icons/hi2";
+import {Link, Route, Routes} from 'react-router-dom';
+import {HiBars3} from "react-icons/hi2";
 import Home from './pages/Home';
 import About from './pages/About';
 import List from './pages/List';
 
 function App() {
-  return (
-    <>
-      <div className="navbar bg-base-100">
-        <div className="flex-1">
-          <Link to="/" className="btn btn-ghost text-xl"><p className="bg-gradient-to-r from-primary to-secondary inline-block text-transparent bg-clip-text pretendard">데브리스트</p></Link>
-        </div>
-        <div className="flex-none gap-2">
-          <div className="form-control">
-            <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
-          </div>
-          <div className="dropdown dropdown-end">
-            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-              <div className="rounded-full">
-                <HiBars3 size={32} />
-              </div>
-            </label>
-            <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52 ibmplexsans">
-              <li><Link to="/">홈</Link></li>
-              <li><Link to="/about">소개</Link></li>
-              <li><Link to="/list">리스트</Link></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/list" element={<List />} />
-      </Routes>
-    </>
-  );
+    return (
+        <>
+            <div className="navbar bg-base-100">
+                <div className="flex-1"><Link to="/" className="btn btn-ghost"><img
+                    src="./icons/devlist_transparent.svg" width="32" height="32"/></Link>
+                </div>
+                <div className="flex-none gap-2">
+                    <div className="form-control">
+                        <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto"/>
+                    </div>
+                    <div className="dropdown dropdown-end">
+                        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                            <div className="rounded-full">
+                                <HiBars3 size={32}/>
+                            </div>
+                        </label>
+                        <ul tabIndex={0}
+                            className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52 ibmplexsans">
+                            <li className="menu-title">데브리스트</li>
+                            <li><Link to="/">홈</Link></li>
+                            <li><Link to="/about">소개</Link></li>
+                            <li><Link to="/list">리스트</Link></li>
+                            <li className="menu-title">We are melotalk family</li>
+                            <li><a href="https://melotalk.vercel.app">멜로톡</a></li>
+                            <li><a href="https://teenagercode.vercel.app">틴에이저코드</a></li>
+                            <li><a href="https://devlist-five.vercel.app">데브리스트</a></li>
+                            <li><a href="https://governmentplus.vercel.app">정부플러스</a></li>
+                            <li><a href="https://toolking.vercel.app">툴킹</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/list" element={<List/>}/>
+            </Routes>
+        </>
+    );
 }
 
 export default App;
