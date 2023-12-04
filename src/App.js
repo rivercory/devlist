ee58f4c40dc533dd5dd77fc1c95bca6339ee35b5
hyeonho64 <hyeonhokang10@kakao.com>
2023-12-04 18:26:48 +0900

    Version 1.1.6

--- src/App.js
+++ src/App.js
@@ -7,43 +7,76 @@ import List from './pages/List';
 
 function App() {
     return (
-        <>
-            <div className="navbar bg-base-100">
-                <div className="flex-1"><Link to="/" className="btn btn-ghost"><img
-                    src="./icons/devlist_transparent.svg" width="32" height="32"/></Link>
-                </div>
-                <div className="flex-none gap-2">
-                    <div className="form-control">
-                        <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto"/>
+        <div className="h-screen relative">
+            <div className="h-full">
+                <div className="navbar bg-base-100 p-1 hidden md:flex">
+                    <div className="flex-1"><Link to="/" className="btn btn-ghost"><img
+                        src="./icons/devlist_transparent.svg" width="32" height="32"/></Link>
                     </div>
-                    <div className="dropdown dropdown-end">
-                        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                            <div className="rounded-full">
-                                <HiBars3 size={32}/>
-                            </div>
-                        </label>
-                        <ul tabIndex={0}
-                            className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52 ibmplexsans">
-                            <li className="menu-title">데브리스트</li>
-                            <li><Link to="/">홈</Link></li>
-                            <li><Link to="/about">소개</Link></li>
-                            <li><Link to="/list">리스트</Link></li>
-                            <li className="menu-title">We are melotalk family</li>
-                            <li><a href="https://melotalk.vercel.app">멜로톡</a></li>
-                            <li><a href="https://teenagercode.vercel.app">틴에이저코드</a></li>
-                            <li><a href="https://devlist-five.vercel.app">데브리스트</a></li>
-                            <li><a href="https://governmentplus.vercel.app">정부플러스</a></li>
-                            <li><a href="https://toolking.vercel.app">툴킹</a></li>
-                        </ul>
+                    <div className="flex-none gap-2">
+                        <div className="form-control">
+                            <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto"/>
+                        </div>
+                        <div className="dropdown dropdown-end">
+                            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                                <div className="rounded-full">
+                                    <HiBars3 size={32}/>
+                                </div>
+                            </label>
+                            <ul tabIndex={0}
+                                className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52 ibmplexsans">
+                                <li className="menu-title">데브리스트</li>
+                                <li><Link to="/">홈</Link></li>
+                                <li><Link to="/about">소개</Link></li>
+                                <li><Link to="/list">리스트</Link></li>
+                                <li className="menu-title">We are melotalk family</li>
+                                <li><a href="https://melotalk.vercel.app">멜로톡</a></li>
+                                <li><a href="https://teenagercode.vercel.app">틴에이저코드</a></li>
+                                <li><a href="https://devlist-five.vercel.app">데브리스트</a></li>
+                                <li><a href="https://governmentplus.vercel.app">정부플러스</a></li>
+                                <li><a href="https://toolking.vercel.app">툴킹</a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/about" element={<About/>}/>
+                    <Route path="/list" element={<List/>}/>
+                </Routes>
+                <div className="btm-nav md:hidden">
+                    <button>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                             stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                  d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/>
+                        </svg>
+                        <span className="btm-nav-label">Home</span>
+                    </button>
+                    <button className="active">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                             stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                        </svg>
+                        <span className="btm-nav-label">Warnings</span>
+                    </button>
+                    <button>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                             stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"/>
+                        </svg>
+                        <span className="btm-nav-label">Statics</span>
+                    </button>
+                </div>
+                <a href="https://hyeonho64.vercel.app">
+                    <div className="absolute bg-gray-200 border-2 border-gray-300 text-xs m-4 p-2 hidden md:block rounded-md bottom-0 right-0">
+                        Copyright 2023. 강현호 all rights reserved.
+                    </div>
+                </a>
             </div>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/about" element={<About/>}/>
-                <Route path="/list" element={<List/>}/>
-            </Routes>
-        </>
+        </div>
     );
 }
 
