007bd7e45e46fc0c10fee80db9a8b755ed4442d2
hyeonho64 <hyeonhokang10@kakao.com>
2023-12-09 21:13:10 +0900

    Version 1.1.8

--- src/pages/List.js
+++ src/pages/List.js
@@ -13,32 +13,32 @@ const List = () => {
 
     return (
         <div className="w-full">
-            <div className="ml-4 mr-4 p-8 rounded-lg bg-primary text-white suit">
+            <div className="ml-4 mt-4 md:mt-0 mr-4 p-8 rounded-lg bg-primary text-white suit">
                 <p className="text-3xl">목록</p>
                 <p>개발에 도움을 주는 웹사이트들입니다.</p>
             </div>
 
-            <div role="tablist" className="mt-4 w-fit tabs tabs-boxed tabs-lg ml-4 pretendard">
-                <a role="tab" className="tab" onClick={() => setFilter()}>
+            <div role="tablist" className="grid gap-2 w-fit ml-4 mt-4 pretendard">
+                <a role="tab" className="tab border-2 rounded-full" onClick={() => setFilter()}>
                     전체
                 </a>
-                <a role="tab" className="tab" onClick={() => setFilter("ui/design")}>
+                <a role="tab" className="tab border-2 rounded-full" onClick={() => setFilter("ui/design")}>
                     UI/디자인
                 </a>
-                <a role="tab" className="tab" onClick={() => setFilter("frontend")}>
+                <a role="tab" className="tab border-2 rounded-full" onClick={() => setFilter("frontend")}>
                     Frontend
                 </a>
-                <a role="tab" className="tab" onClick={() => setFilter("etc")}>
+                <a role="tab" className="tab border-2 rounded-full" onClick={() => setFilter("etc")}>
                     기타
                 </a>
             </div>
 
-            <div className="grid grid-cols-4 p-4 gap-4 w-full">
+            <div className="grid grid-cols-1 md:grid-cols-4 p-4 gap-4 w-fit">
                 {filteredProjects.map(project => {
                     return (
                         <a href={project.link} target="_blank">
                             <div className="card bg-base-100 card-bordered border-2">
-                                <figure><img src={project.img} alt="Shoes" /></figure>
+                                <figure><img className="p-6" src={project.img} alt="Shoes" /></figure>
                                 <div className="card-body">
                                     <h2 className="card-title">
                                         {project.name}
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
       },
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [
+      require("daisyui"),
+      require("tailwind-scrollbar-hide")
+  ],
 }
 
